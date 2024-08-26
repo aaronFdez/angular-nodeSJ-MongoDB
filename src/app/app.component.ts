c894d5ca7db1f25d1471b5bb79a436f056555e45
Aaron Fernandez <aaron.fernandez@alisys.net>
2024-08-26 12:27:58 +0200

    añadir componente videojuego

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {VideojuegoComponent} from "./videojuego/videojuego.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, VideojuegoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
