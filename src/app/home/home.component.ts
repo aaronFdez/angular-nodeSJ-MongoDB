ec5b57767cff317c560b4fd83452e08e77fa3bee
Aaron Fernandez <aaron.fernandez@alisys.net>
2024-08-28 13:32:33 +0200

    uso de ng-templates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { RouterLink } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
-  
-  ngOnInit() {
+export class HomeComponent implements OnInit {
+  public isIdentificated: boolean;
 
+
+  constructor() {
+    this.isIdentificated = false;
   }
- }
+
+  login() {
+      this.isIdentificated = !this.isIdentificated;
+  }
+
+  ngOnInit() {
+  }
+}
