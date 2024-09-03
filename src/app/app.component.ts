cfdd20c467d3392dfc535806744ffde2809ec882
aaron.fernandez <aaron.fernandez@alisys.net>
2024-09-03 12:13:32 +0200

    se añade componente de contacto, una ruta y se añade al global

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-
+import {CalculadoraPipe} from "./pipes/calculadora.pipe";
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CalculadoraPipe, DatePipe],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Mi Aplicación Angular';
+
+  date = new Date();
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { BotinesComponent } from "./botines/botines.component";
 import { CursosComponent } from "./cursos/cursos.component";
 import { HomeComponent } from "./home/home.component";
 import { ExternalComponent} from "./external/external.component";
+import {ContactoComponent} from "./contacto/contacto.component";
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -13,5 +14,6 @@ export const routes: Routes = [
     { path: 'cursos/:nombre', component: CursosComponent },
     { path: 'botines', component: BotinesComponent },
     { path: 'external', component: ExternalComponent },
+    { path: 'contacto', component: ContactoComponent },
     { path: '**', component: HomeComponent }
 ];
